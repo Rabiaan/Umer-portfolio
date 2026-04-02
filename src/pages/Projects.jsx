9f541bd1d2f18205a6ca1295d9db13e71c6d7e7d
Syed Rabiaan <syedrabiaan@gmail.com>
2026-04-02 19:58:33 +0500

    Add KRONE animation project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -50,6 +50,13 @@ export default function Projects() {
       embedUrl: "https://www.behance.net/embed/project/219046293?ilo0=1",
       behanceUrl: "https://www.behance.net/gallery/219046293/Kingtox",
     },
+    {
+      id: 7,
+      title: "KRONE",
+      category: "Animations",
+      embedUrl: "https://www.behance.net/embed/project/211855881?ilo0=1",
+      behanceUrl: "https://www.behance.net/gallery/211855881/KRONE",
+    },
   ];
 
   const filteredProjects = filter === "All" ? projects : projects.filter(p => p.category === filter);
